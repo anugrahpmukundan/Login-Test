801cb2e54044048717bac52f26584bf9698d9b0c
Anugrah <44465752+9539763597@users.noreply.github.com>
2022-06-20 15:02:18 +0530

    Views created

---
+++ app/src/main/java/com/anugrah/logintest/view/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.anugrah.logintest.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anugrah.logintest.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
--- app/src/main/java/com/anugrah/logintest/view/LoginActivity.kt
+++ app/src/main/java/com/anugrah/logintest/view/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.anugrah.logintest
+package com.anugrah.logintest.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.anugrah.logintest.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
